adacd7703f8fa8c12d3a1338f5f00205b66ddd80
prithivi5667 <prithivi5667@gmail.com>
2021-12-18 10:26:36 +0530

    d

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
-                'Content-Type': 'application/json',
+                "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
                 "Access-Control-Allow-Origin": "*"
             },
             credentials: true,
